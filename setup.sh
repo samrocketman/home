13984249dc0e9c8485991a40c9b159b842397f5d
Sam Gleske <sam.mxracer@gmail.com>
2014-08-03 17:58:11 -0400

    setup script

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Sam Gleske home setup script
+#Ubuntu 14.04 LTS \n \l
+#Linux 3.13.0-24-generic x86_64
+#GNU bash, version 4.3.8(1)-release (x86_64-pc-linux-gnu)
+if [ ! -d "${HOME}/git/home" ];then
+  echo "ERR: home repo not cloned in ~/git?" >&2
+  exit 1
+fi
+
+ln -s ~/git/home/dotfiles/.[a-z]* ~/
+if [ ! -e ~/bin ];then
+  ln -s ~/git/home/bin ~/bin
+fi
+
+grep '.bashrc_custom' ~/.bashrc || echo '. ~/.bashrc_custom' > ~/.bashrc
