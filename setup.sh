e5037b9931a8ec6f5a5e50e96bddea41d701583f
Sam Gleske <samuel_gleske@cable.comcast.com>
2014-08-08 10:09:07 -0400

    setup-script: override PROJECT_HOME added
    Now the location in which the git repository is checked out can be
    overridden.

--- setup.sh
+++ setup.sh
@@ -3,14 +3,17 @@
 #Ubuntu 14.04 LTS \n \l
 #Linux 3.13.0-24-generic x86_64
 #GNU bash, version 4.3.8(1)-release (x86_64-pc-linux-gnu)
-if [ ! -d "${HOME}/git/home" ];then
+
+PROJECT_HOME="${PROJECT_HOME:-${HOME}/git/home}"
+
+if [ ! -d "" ];then
   echo "ERR: home repo not cloned in ~/git?" >&2
   exit 1
 fi
 
-ln -s ~/git/home/dotfiles/.[a-z]* ~/
+ln -s "${PROJECT_HOME}"/dotfiles/.[a-z]* ~/
 if [ ! -e "${HOME}/bin" ];then
-  ln -s ~/git/home/bin ~/bin
+  ln -s "${PROJECT_HOME}"/bin ~/bin
 fi
 
 grep '.bashrc_custom' ~/.bashrc &> /dev/null || echo '. ~/.bashrc_custom' >> ~/.bashrc
