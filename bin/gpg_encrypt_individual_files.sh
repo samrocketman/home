da9ce0b43ecf36863e5a01faa7ee0af4b8cd8a79
Sam Gleske <sam.mxracer@gmail.com>
2014-08-12 18:11:12 -0400

    bin/gpg*: Code style and cleanup
    Adding gpg_opts env variable.
    No longer than 80 chars.
    Consistent loop do spaces.
    Consistent conditional then spaces.

--- bin/gpg_encrypt_individual_files.sh
+++ bin/gpg_encrypt_individual_files.sh
@@ -13,37 +13,48 @@
 #USAGE
 #  gpg_encrypt_individual_files.sh dir/
 
-#a space separated list of recipient key IDs; this value can be overridden by environment
+#a space separated list of recipient key IDs
+#this value can be overridden by environment
 recipient_list="${recipient_list:-}"
 
-#remove the original unencrypted file?; this value can be overridden by environment
+#remove the original unencrypted file?
+#this value can be overridden by environment
 remove_original="${remove_original:-true}"
 
-#skip files that already have an encrypted equivalent? i.e. this will replace the encrypted file equivalent if it already exists; this value can be overridden by environment
+#skip files that already have an encrypted equivalent?
+#i.e. this will replace the encrypted file equivalent if it already exists
+#this value can be overridden by environment
 skip_encrypted="${skip_encrypted:-false}"
 
-#create checksums for the files in each directory.  Each directory will contain a file call sha1sum.txt; this value can be overridden by environment
+#create checksums for the files in each directory.
+#Each directory will contain a file call sha1sum.txt
+#this value can be overridden by environment
 create_checksums="${create_checksums:-true}"
 
 #Do you want to sign the files too?; this value can be overridden by environment
 sign_encrypted_file="${sign_encrypted_file:-false}"
 
+#Additional options for gpg commands
+#gpg_opts="${gpg_opts:---use-agent}"
+
 #DO NOT EDIT ANY MORE VARIABLES
-#this will individually encrypt all files in the folder; this value can be overridden by environment
-if [ -z "${folder_to_encrypt}" ];then
+#this will individually encrypt all files in the folder
+#this value can be overridden by environment
+if [ -z "${folder_to_encrypt}" ]; then
   folder_to_encrypt="${1}"
 fi
 
-#if the end of the file or name matches an ignore_rule then it will not be encrypted; this value can be overridden by environment
-if [ "${#ignore_rules[@]}" -eq "0" ];then
-  ignore_rules=('.gpg' 'sha1sum.txt' '.checksumrequired')
+#if the end of the file or name matches an ignore_rule,
+#then it will not be encrypted; this value can be overridden by environment
+if [ "${#ignore_rules[@]}" -eq "0" ]; then
+  ignore_rules=('.gpg' 'sha1sum.txt' '.checksumrequired' '.sig')
 fi
 
 
-if [ -z "${folder_to_encrypt}" -o ! -d "${folder_to_encrypt}" ];then
+if [ -z "${folder_to_encrypt}" -o ! -d "${folder_to_encrypt}" ]; then
   echo "Must provide a valid folder as an argument!"
   exit 1
-elif [ -z "${recipient_list}" ];then
+elif [ -z "${recipient_list}" ]; then
   echo "recipient_list environment variable is not set!"
   echo "You should set it in ~/.bashrc and/or ~/.bash_profile."
   echo "It is a space separated list of GPG key IDs."
@@ -61,10 +72,13 @@ for x in ${recipient_list};do recipients="${recipients} --recipient ${x}";done
 
 #build the ignore rules for the find command
 ignore_expression=""
-for x in ${ignore_rules[@]};do ignore_expression="${ignore_expression} -path *${x} -prune -o "; done
+for x in ${ignore_rules[@]}; do
+  ignore_expression="${ignore_expression} -path *${x} -prune -o "
+done
 
 #use find command to find files to encrypt
-find "${folder_to_encrypt}" ${ignore_expression} -type f -print | while read x;do
+find "${folder_to_encrypt}" ${ignore_expression} -type f -print \
+  | while read x; do
   dir="${x%/*}"
   file="${x##*/}"
   (
@@ -74,15 +88,16 @@ find "${folder_to_encrypt}" ${ignore_expression} -type f -print | while read x;d
     cd "${dir}"
     #remove encrypted file equivalent if it exists
     ! ${skip_encrypted} && [ -f "${file}.gpg" ] && rm -f -- "${file}.gpg"
-    if ! ${skip_encrypted} || [ "${skip_encrypted}" = "true" -a ! -f "${file}.gpg" ];then
+    if ! ${skip_encrypted} \
+      || [ "${skip_encrypted}" = "true" -a ! -f "${file}.gpg" ]; then
       #sign (-d) encrypt (-e) the file (output is filename.gpg)
-      if ${sign_encrypted_file};then
-        gpg -s -e ${recipients} -- "${file}"
+      if ${sign_encrypted_file}; then
+        gpg ${gpg_opts} -s -e ${recipients} -- "${file}"
       else
-        gpg -e ${recipients} -- "${file}"
+        gpg ${gpg_opts} -e ${recipients} -- "${file}"
       fi
       echo -n "encrypted ${x}"
-      if ${remove_original};then
+      if ${remove_original}; then
         rm -f -- "${file}"
         echo " and removed original."
       else
@@ -93,8 +108,10 @@ find "${folder_to_encrypt}" ${ignore_expression} -type f -print | while read x;d
   )
 done
 ${create_checksums} && echo "Checksumming files in ${folder_to_encrypt}..."
-#create a checksum of each file in the folder but ONLY do it to encrypted files that have changed (i.e. has a *.gpg.checksumrequired file)
-${create_checksums} && find "${folder_to_encrypt}" -type d | while read x;do
+#create a checksum of each file in the folder
+#but ONLY do it to encrypted files that have changed
+#i.e. has a *.gpg.checksumrequired file
+${create_checksums} && find "${folder_to_encrypt}" -type d | while read x; do
   (
     #subshell
     #exit subshell on first error
@@ -102,10 +119,13 @@ ${create_checksums} && find "${folder_to_encrypt}" -type d | while read x;do
     cd "${x}"
     #create sha1sum.txt file if it doesn't exist
     [ ! -f "sha1sum.txt" ] && touch sha1sum.txt
-    find . -maxdepth 1 -type f -name '*.gpg.checksumrequired' -printf '%f\n' | while read esum;do
+    find . -maxdepth 1 -type f -name '*.gpg.checksumrequired' -printf '%f\n' \
+      | while read esum; do
       #delete the old entry from the sha1sum.txt file
       echo "Checksumming ${x}/${esum%\.checksumrequired}"
-      expression="$(echo "${esum%\.checksumrequired}" | sed 's/\[/\\\[/g' | sed 's/\]/\\\]/g')"
+      expression="$(echo "${esum%\.checksumrequired}" \
+        | sed 's/\[/\\\[/g' \
+        | sed 's/\]/\\\]/g')"
       sed -i "/\w\s\+${expression}/ d" sha1sum.txt
       #append the new sum to the sha1sum.txt file
       sha1sum -- "${esum%\.checksumrequired}" >> sha1sum.txt
