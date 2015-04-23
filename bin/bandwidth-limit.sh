6dcfc239a6c884381cc63327455319cc1c0b6a4f
Sam Gleske <sam.mxracer@gmail.com>
2015-04-22 20:17:22 -0400

    Adding my own changes to the bandwidth limiting
    script.

--- bin/bandwidth-limit.sh
+++ bin/bandwidth-limit.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#http://www.iplocation.net/tools/traffic-control.php
+#with modifications from Sam Gleske
 #
 #  tc uses the following units when passed as a parameter.
 #  kbps: Kilobytes per second
@@ -18,8 +20,8 @@
 # Name of the traffic control command.
 TC=/sbin/tc
 
-# The network interface we're planning on limiting bandwidth.
-IF=eth0             # Interface
+# The network interface of the default route
+IF="$(ip ro | awk '$1 == "default" { print $5 }')"
 
 # Download limit (in mega bits)
 DNLD=1mbit          # DOWNLOAD Limit
@@ -27,11 +29,11 @@ DNLD=1mbit          # DOWNLOAD Limit
 # Upload limit (in mega bits)
 UPLD=1mbit          # UPLOAD Limit
 
-# IP address of the machine we are controlling
-IP=216.3.128.12     # Host IP
+# IP address of the default route
+IP="$(ip -o ro get $(ip ro | awk '$1 == "default" { print $3 }') | awk '{print $5}')/32"     # Host IP
 
 # Filter options for limiting the intended interface.
-U32="$TC filter add dev $IF protocol ip parent 1:0 prio 1 u32"
+U32="$TC filter add dev $IF protocol ip parent 1: prio 1 u32"
 
 start() {
 
@@ -42,8 +44,8 @@ start() {
     $TC qdisc add dev $IF root handle 1: htb default 30
     $TC class add dev $IF parent 1: classid 1:1 htb rate $DNLD
     $TC class add dev $IF parent 1: classid 1:2 htb rate $UPLD
-    $U32 match ip dst $IP/32 flowid 1:1
-    $U32 match ip src $IP/32 flowid 1:2
+    $U32 match ip dst $IP flowid 1:1
+    $U32 match ip src $IP flowid 1:2
 
 # The first line creates the root qdisc, and the next two lines
 # create two child qdisc that are to be used to shape download
