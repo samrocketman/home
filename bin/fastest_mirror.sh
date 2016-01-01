8d555ec3cb9e2537d69c868d8626b1ede4e93b85
Sam Gleske <sam.mxracer@gmail.com>
2015-12-31 19:50:28 -0800

    better help and options for fastest_mirror.sh

--- bin/fastest_mirror.sh
+++ bin/fastest_mirror.sh
@@ -12,6 +12,63 @@
 #Description:
 #  Find the fastest mirror for Ubuntu updates
 
+#check for help option
+for x in "$@"; do
+  case $x in
+    -h|--help)
+      #helpdoc was created by reading source of man pages crontab(1) and
+      #apt-cache(8)
+      man -l - <<EOF
+.TH fastest_mirror.sh 1 "28 November 2015"
+.UC 4
+.SH NAME
+.B fastest_mirror.sh
+\\- Find the fastest mirror for Ubuntu updates.
+.SH SYNOPSIS
+.B fastest_mirror.sh
+[
+.B \\-u
+] [
+.B \\-\\-update
+]
+.SH DESCRIPTION
+.I fastest_mirror.sh
+is a program used to determine the fastest Ubuntu update mirrors for this
+network.  It uses
+.IR netselect (1)
+to inquire latency and throughput for mirrors found at
+https://launchpad.net/ubuntu/+archivemirrors relative to the current network of
+this machine.
+.SH "OPTIONS"
+.PP
+If this script is run without options then, by default, it will print out the
+fastest mirrors without modifying any system files.
+.PP
+\\fB\\-h\\fR, \\fB\\-\\-help\\fR
+.RS 4
+Show this help document.
+.RE
+.PP
+\\fB\\-u\\fR, \\fB\\-\\-update\\fR
+.RS 4
+Update
+.I sources.list
+with the fastest mirror.  NOTE: this option modifies system files.
+.RE
+.SH "SEE ALSO"
+\\fBnetselect\\fR(1),
+\\fBsources.list\\fR(5)
+.br
+http://askubuntu.com/questions/39922/how-do-you-select-the-fastest-mirror-from-the-command-line/141536#141536
+.br
+https://launchpad.net/ubuntu/+archivemirrors
+.SH "AUTHOR"
+Sam Gleske https://sam.gleske.net/
+EOF
+      exit 1
+      ;;
+  esac
+done
 
 if [ ! -e '/usr/bin/netselect' ]; then
   echo 'Error: netselect package missing.'
@@ -21,8 +78,55 @@ fi
 
 if [ ! 'root' = "$USER" ]; then
   echo 'Launching script as administrator.'
-  sudo "$0" "$@"
+  sudo -- "$0" "$@"
   exit $?
 fi
 
-netselect -v -s200 -t20 `wget -q -O- https://launchpad.net/ubuntu/+archivemirrors | grep -P -B8 "statusUP|statusSIX" | grep -o -P "(f|ht)tp.*\"" | tr '"\n' '  '`
+#option parsing
+auto_update=false
+while [ "$#" -gt '0' ]; do
+  case $1 in
+    -h|--help)
+      showhelp=true
+      break
+      ;;
+    -u|--update)
+      auto_update=true
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+
+if ${auto_update}; then
+  #determine if Ubuntu
+  if type -f lsb_release &> /dev/null && [ ! 'Ubuntu' = "$(lsb_release -si)" ]; then
+    echo "Not Ubuntu so there's not much reason to run this script."
+    exit 1
+  fi
+
+  if [ ! -e "/etc/apt/sources.list.old" ]; then
+    cp /etc/apt/sources.list /etc/apt/sources.list.old
+  fi
+
+  #check up to 200 different servers for fastest internet speeds
+  #choose the fastest one as the $fastest_mirror
+  fastest_mirror="$(netselect -v -s200 -t20 $(wget -q -O- https://launchpad.net/ubuntu/+archivemirrors | grep -P -B8 "statusUP|statusSIX" | grep -o -P "(f|ht)tp.*\"" | tr '"\n' '  ') | head -n1 | awk '{print $2}' )"
+
+  #create a temporary file with the fastest mirror
+  sed "s#http://us.archive.ubuntu.com/ubuntu/#${fastest_mirror}#" /etc/apt/sources.list.old > /tmp/sources.list
+
+  if ! diff /tmp/sources.list /etc/apt/sources.list &> /dev/null; then
+    echo "Updating sources.list to new fastest mirror: ${fastest_mirror}"
+    mv /tmp/sources.list /etc/apt/sources.list
+  else
+    echo "Fastest mirror remains: ${fastest_mirror}"
+    rm /tmp/sources.list
+  fi
+  echo 'The originally installed sources.list is located at /etc/apt/sources.list.old'
+else
+  netselect -v -s200 -t20 $(wget -q -O- https://launchpad.net/ubuntu/+archivemirrors | grep -P -B8 "statusUP|statusSIX" | grep -o -P "(f|ht)tp.*\"" | tr '"\n' '  ')
+fi
