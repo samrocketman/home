8f0764ae05fc6219d787dc2151a84f5519fca00e
Sam Gleske <sam.mxracer@gmail.com>
2016-01-23 14:26:56 -0800

    say job done script

---
+++ bin/say_job_done.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Sam Gleske
+#Sat Jan 23 14:15:22 PST 2016
+#Mac OSX 10.11.2
+#Darwin 15.2.0 x86_64
+#DESCRIPTION:
+#  Script to say when the job is done.  Useful in programming loops and when
+#  programs exit without needing to regularly check the terminal window.  It
+#  will say when the job is done.
+
+say "Jobb done."
