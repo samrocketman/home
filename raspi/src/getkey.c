d59758a6044d671229dc4f055aa7004ec4f41097
Sam Gleske <sam.mxracer@gmail.com>
2016-05-16 17:53:06 -0700

    getkey.c inverse return logic

--- raspi/src/getkey.c
+++ raspi/src/getkey.c
@@ -6,6 +6,13 @@ http://linux.die.net/man/3/fd_zero
 http://stackoverflow.com/questions/23068559/linux-input-device-reading-ioctleviocgkey-versus-readinput-event
 http://www.linuxjournal.com/article/6429?page=0,0 - Using the Input Subsystem, Part II
 http://man7.org/linux/man-pages/man2/open.2.html
+
+Example usage:
+getkey /dev/input/by-path/*-kbd lshift
+
+Logic is inversed.  So will return true when shift key is not pressed else return non-zero.
+
+This is good for bash logic because it means we can detect a shift key or a program error.
 */
 
 #include <stdlib.h>
@@ -44,5 +51,5 @@ int main ( int argc, char *argv[], char *env[] )
     int keyb = key_map[key/8];  //  The key we want (and the seven others arround it)
     int mask = 1 << (key % 8);  //  Put a one in the same column as out key state will be in;
 
-    return !(keyb & mask);  //  Returns true if pressed otherwise false
+    return (keyb & mask);  //  Returns true if pressed otherwise false
 }
