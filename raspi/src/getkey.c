c3fb504815349e98aa784f275586e8032d0c0c0f
Sam Gleske <sam.mxracer@gmail.com>
2016-05-16 17:47:32 -0700

    getkey.c device path as argument
    Allows to take advantage of shell globbing.

--- raspi/src/getkey.c
+++ raspi/src/getkey.c
@@ -1,6 +1,11 @@
 /*
 Retrieved Mon 16 May 17:04:18 PDT 2016
 http://stackoverflow.com/questions/3649874/how-to-get-keyboard-state-in-linux
+http://stackoverflow.com/questions/448944/c-non-blocking-keyboard-input
+http://linux.die.net/man/3/fd_zero
+http://stackoverflow.com/questions/23068559/linux-input-device-reading-ioctleviocgkey-versus-readinput-event
+http://www.linuxjournal.com/article/6429?page=0,0 - Using the Input Subsystem, Part II
+http://man7.org/linux/man-pages/man2/open.2.html
 */
 
 #include <stdlib.h>
@@ -11,27 +16,26 @@ http://stackoverflow.com/questions/3649874/how-to-get-keyboard-state-in-linux
 
 void usage ( int argc, char *argv[] )
 {
-    printf("Usage:\n\t%s key\n\nvalid keys are:\n\tlshift\t- Left Shift key\n" , argv[0]);
+    printf("Usage:\n\t%s device key\n\ne.g. path - /dev/input/by-path/platform-3f980000.usb-usb-0:1.2:1.0-event-kbd\n\nvalid keys are:\n\tlshift\t- Left Shift key\n" , argv[0]);
 
     exit(EXIT_FAILURE);
 }
 
 int main ( int argc, char *argv[], char *env[] )
 {
-    if ( argc != 2 )    usage(argc, argv);
+    if ( argc != 3 )    usage(argc, argv);
 
     int key;
 
-    if ( strcmp(argv[1], "lshift") == 0 )       key = KEY_LEFTSHIFT;
-    else if ( strcmp(argv[1], "rshift") == 0 )  key = KEY_RIGHTSHIFT;
-    else if ( strcmp(argv[1], "lalt") == 0 )    key = KEY_LEFTALT;
-    else if ( strcmp(argv[1], "ralt") == 0 )    key = KEY_RIGHTALT;
-    else if ( strcmp(argv[1], "lctrl") == 0 )   key = KEY_LEFTCTRL;
-    else if ( strcmp(argv[1], "rctrl") == 0 )   key = KEY_RIGHTCTRL;
+    if ( strcmp(argv[2], "lshift") == 0 )       key = KEY_LEFTSHIFT;
+    else if ( strcmp(argv[2], "rshift") == 0 )  key = KEY_RIGHTSHIFT;
+    else if ( strcmp(argv[2], "lalt") == 0 )    key = KEY_LEFTALT;
+    else if ( strcmp(argv[2], "ralt") == 0 )    key = KEY_RIGHTALT;
+    else if ( strcmp(argv[2], "lctrl") == 0 )   key = KEY_LEFTCTRL;
+    else if ( strcmp(argv[2], "rctrl") == 0 )   key = KEY_RIGHTCTRL;
 
 
     FILE *kbd = fopen("/dev/input/by-path/platform-3f980000.usb-usb-0:1.2:1.0-event-kbd", "r");
-
     char key_map[KEY_MAX/8 + 1];    //  Create a byte array the size of the number of keys
 
     memset(key_map, 0, sizeof(key_map));    //  Initate the array to zero's
@@ -41,5 +45,4 @@ int main ( int argc, char *argv[], char *env[] )
     int mask = 1 << (key % 8);  //  Put a one in the same column as out key state will be in;
 
     return !(keyb & mask);  //  Returns true if pressed otherwise false
-
 }
