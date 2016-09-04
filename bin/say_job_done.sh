98ecb99f9698372cac533d6e2be86d7f01ee3f41
Sam Gleske <sam.mxracer@gmail.com>
2016-09-04 12:16:13 -0700

    use espeak when say command not available

--- bin/say_job_done.sh
+++ bin/say_job_done.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 #Sam Gleske
 #Sat Jan 23 14:15:22 PST 2016
+
+#say command
 #Mac OSX 10.11.2
 #Darwin 15.2.0 x86_64
+
+#espeak command
+#Ubuntu 16.04.1 LTS
+#Linux 4.4.0-36-generic x86_64
+
 #DESCRIPTION:
 #  Script to say when the job is done.  Useful in programming loops and when
 #  programs exit without needing to regularly check the terminal window.  It
 #  will say when the job is done.
 
-say "Jobb done."
+if type -p espeak &> /dev/null;then
+  espeak <<< "Jobb done."
+elif type -p say &> /dev/null;then
+  say "Jobb done."
+else
+  echo "No speaking command available." 1>&2
+  exit 1
+fi
