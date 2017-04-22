bb07c031950fe401f321c851571af4890e2c2f69
Sam Gleske <sam.mxracer@gmail.com>
2017-04-21 17:45:26 -0700

    Remove jq dependency from jenkins_wait_job.sh

--- bin/jenkins_wait_job.sh
+++ bin/jenkins_wait_job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-while $(jenkins-call-url ${1%/}/api/json | jq -r '.building'); do
+while $(jenkins-call-url ${1%/}/api/json | python -c 'import sys,json;print str(json.load(sys.stdin)["building"]).lower()'); do
   sleep 5
 done
 
-RESULT=$(jenkins-call-url ${1%/}/api/json | jq -r '.result')
+RESULT=$(jenkins-call-url ${1%/}/api/json | python -c 'import sys,json;print json.load(sys.stdin)["result"]')
 
 [ "${RESULT}" = 'SUCCESS' ] && \
   say_job_done.sh 'Jobb success.' || \
