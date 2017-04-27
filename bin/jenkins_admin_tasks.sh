ce8dd57db7db81a12b9c750be7a79e735893ced7
Sam Gleske <sam.mxracer@gmail.com>
2017-04-26 22:53:56 -0700

    jenkins_admin_tasks now uses jenkins-call-url
    Additionally, added script console script to kill stale jobs which are
    running longer than a day.

--- bin/jenkins_admin_tasks.sh
+++ bin/jenkins_admin_tasks.sh
@@ -10,7 +10,9 @@ JENKINS_WEB="${JENKINS_WEB:-http://localhost:8080/}"
 
 #remove trailing slash
 JENKINS_WEB="${JENKINS_WEB%/}"
-CURL="${CURL:-curl}"
+
+export JENKINS_WEB
+export JENKINS_CALL_ARGS="-m POST --data-string script= ${JENKINS_WEB}/scriptText -d"
 
 #
 # SCRIPT CONSOLE SCRIPTS
@@ -26,6 +28,81 @@ EOF
 function script_disable_shutdown_mode() {
   cat <<'EOF'
 Jenkins.instance.doCancelQuietDown()
+null
+EOF
+}
+
+function script_kill_day_old_jobs() {
+  cat <<'EOF'
+/*
+    Copyright (c) 2015-2017 Sam Gleske - https://github.com/samrocketman/jenkins-script-console-scripts
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy of
+    this software and associated documentation files (the "Software"), to deal in
+    the Software without restriction, including without limitation the rights to
+    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+    the Software, and to permit persons to whom the Software is furnished to do so,
+    subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+    FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+    COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+    IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+/*
+   This script kills all builds which have been running longer than one day.
+   This helps to keep a Jenkins instance healthy and executors free from
+   existing stale build runs.
+
+   Supported types to kill:
+       FreeStyleBuild
+       WorkflowRun (Jenkins Pipelines)
+*/
+
+import hudson.model.FreeStyleBuild
+import hudson.model.Job
+import hudson.model.Result
+import hudson.model.Run
+import java.util.Calendar
+import jenkins.model.Jenkins
+import org.jenkinsci.plugins.workflow.job.WorkflowRun
+
+//24 hours in a day, 3600 seconds in 1 hour, 1000 milliseconds in 1 second
+long time_in_millis = 24*3600*1000
+Calendar rightNow = Calendar.getInstance()
+
+Jenkins.instance.getAllItems(Job.class).findAll { Job job ->
+    job.isBuilding()
+}.collect { Job job ->
+    job.getBuilds().findAll { Run run ->
+        run.isBuilding() && ((rightNow.getTimeInMillis() - run.getStartTimeInMillis()) > time_in_millis)
+    }
+}.each { List listOfRuns ->
+    if(listOfRuns) {
+        //the listOfRuns is not empty
+        listOfRuns.each { Run item ->
+            if(item instanceof WorkflowRun) {
+                WorkflowRun run = (WorkflowRun) item
+                run.doKill()
+                println "Killed ${run}"
+            } else if(item instanceof FreeStyleBuild) {
+                FreeStyleBuild run = (FreeStyleBuild) item
+                run.executor.interrupt(Result.ABORTED)
+                println "Killed ${run}"
+            } else {
+                println "WARNING: Don't know how to handle ${item.class}"
+            }
+        }
+    }
+}
+
+//null means there will be no return result for the script
 null
 EOF
 }
@@ -51,7 +128,7 @@ else if(Jenkins.instance.isQuietingDown() && !RestartListener.isAllReady()) {
 }
 else {
     //println 'Jenkins is ready for use.'
-    println 'ready'
+    println 'shutdown-mode-not-enabled'
 }
 
 null
@@ -64,99 +141,52 @@ EOF
 
 function jenkins_script_console() {
   echo "Calling jenkins_script_console $1" >&2
-  ${CURL} --data-urlencode "script=$(eval "$1")" ${JENKINS_WEB}/scriptText
+  jenkins-call-url <("$1")
 }
 
-#CSRF protection support
-function is_crumbs_enabled() {
-  use_crumbs="$( $CURL -s ${JENKINS_WEB}/api/json?pretty=true 2> /dev/null | python -c 'import sys,json;exec "try:\n  j=json.load(sys.stdin)\n  print str(j[\"useCrumbs\"]).lower()\nexcept:\n  pass"' )"
-  if [ "${use_crumbs}" = "true" ]; then
-    return 0
-  fi
-  return 1
-}
-
-#CSRF protection support
-function get_crumb() {
-  ${CURL} -s ${JENKINS_WEB}/crumbIssuer/api/json | python -c 'import sys,json;j=json.load(sys.stdin);print j["crumbRequestField"] + "=" + j["crumb"]'
-}
-
-#CSRF protection support
-function csrf_set_curl() {
-  if is_crumbs_enabled; then
-    if [ ! "${CSRF_CRUMB}" = "$(get_crumb)" ]; then
-      if [ -n "${CSRF_CRUMB}" ]; then
-        #remove existing crumb value from curl command
-        CURL="$(echo "${CURL}" | sed "s/ -d ${CSRF_CRUMB}//")"
-      fi
-      export CSRF_CRUMB="$(get_crumb)"
-      export CURL="${CURL} -d ${CSRF_CRUMB}"
-      echo "Using crumbs for CSRF support." >&2
-    elif ! echo "${CURL}" | grep -F "${CSRF_CRUMB}" &> /dev/null; then
-      export CURL="${CURL} -d ${CSRF_CRUMB}"
-      echo "Using crumbs for CSRF support." >&2
-    fi
-  fi
-}
-
-function is_auth_enabled() {
-  no_authentication="$( $CURL -s ${JENKINS_WEB}/api/json?pretty=true 2> /dev/null | python -c 'import sys,json;exec "try:\n  j=json.load(sys.stdin)\n  print str(j[\"useSecurity\"]).lower()\nexcept:\n  pass"' )"
-  #check if authentication is required.;
-  #if the value of no_authentication is anything but false; then assume authentication
-  if [ ! "${no_authentication}" = "false" ]; then
-    echo -n "Authentication required..." >&2
-    if [ -n "${JENKINS_AUTH}" ]; then
-      echo "DONE" >&2
-      return 0
-    else
-      echo "FAILED" >&2
-      echo "Could not set authentication." >&2
-      echo "Missing environment variable: JENKINS_AUTH" >&2
-      exit 1
-    fi
-  fi
-  return 1
-}
-
-case "$1" in
-  -h|--help)
-    cat <<- "EOF"
+if grep -- '-h\|--help' <<< "$*" &> /dev/null; then
+  cat <<- "EOF"
 SYNOPSIS
   jenkins_admin_tasks.sh [command]
 
 DESCRIPTION
-  Basic admin tasks.
+  Basic Jenkins server admin tasks.  Commands can be chained together.
 
 COMMANDS
 
-  --enable-shutdown-mode, -s
-  --disable-shutdown-mode, +s
-  --safe-restart
+  --debug                      More verbose output.
+  --disable-shutdown-mode, +s  Disable shutdown mode.
+  --enable-shutdown-mode, -s   Enable shutdown mode.
+  --safe-restart               Restart if Jenkins is in shutdown mode and
+                               there's no in-progress executors.
 
 ENVIRONMENT
 
-  JENKINS_AUTH - user:password
   JENKINS_WEB - http://example.com/jenkins
 EOF
-    exit 1
-esac
-
-if is_auth_enabled; then
-  export CURL="curl -s --user $JENKINS_AUTH"
+  exit 1
 fi
-csrf_set_curl
 
-case "$1" in
-  --enable-shutdown-mode|-s)
-    jenkins_script_console script_enable_shutdown_mode
-    ;;
-  --disable-shutdown-mode|+s)
-    jenkins_script_console script_disable_shutdown_mode
-    ;;
-  --safe-restart)
-    jenkins_script_console script_safe_restart
-    ;;
-  *)
-    echo 'Invalid command, see -h or --help' >&2
-    exit 1
-esac
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --disable-shutdown-mode|+s)
+      jenkins_script_console script_disable_shutdown_mode
+      ;;
+    --enable-shutdown-mode|-s)
+      jenkins_script_console script_enable_shutdown_mode
+      ;;
+    --kill-day-old-jobs|-k)
+      jenkins_script_console script_kill_day_old_jobs
+      ;;
+    --safe-restart|-r)
+      jenkins_script_console script_safe_restart
+      ;;
+    --debug)
+      export JENKINS_CALL_ARGS="-v -v ${JENKINS_CALL_ARGS}"
+      ;;
+    *)
+      echo 'Invalid command, see -h or --help' >&2
+      exit 1
+  esac
+  shift
+done
