245b4f9123ef35373a58ba7de0345e1363aa5d51
Sam Gleske <sam.mxracer@gmail.com>
2017-06-27 20:39:41 -0700

    Add CURL_OPTS to jenkins_wait_reboot script

--- bin/jenkins_wait_reboot_done.sh
+++ bin/jenkins_wait_reboot_done.sh
@@ -3,5 +3,5 @@
 url="${1:-${JENKINS_WAIT_REBOOT_DEFAULT}}"
 endpoint=${url#*//}
 endpoint=${endpoint%%.*}
-while [ "503" = "$(curl -siI -w "%{http_code}\\n" -o /dev/null ${url})" ];do echo -n '.';sleep 1;done
+while [ "503" = "$(curl ${CURL_OPTS} -siI -w "%{http_code}\\n" -o /dev/null ${url})" ];do echo -n '.';sleep 1;done
 say_job_done.sh "${endpoint} ready."
