d7fd5efe7283456b67b077a112255cd6aac249c7
Sam Gleske <sam.mxracer@gmail.com>
2018-09-09 14:09:28 -0700

    Install or update docker-compose
    The install process is idempotent and will only download docker-compose
    if it is out of date.

--- bin/install-docker-compose.sh
+++ bin/install-docker-compose.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 # Created by Sam Gleske (GitHub @samrocketman)
-# Downloads the latest docker-compose binary and verifies the checksum.
-
-DESTINATION="${DESTINATION:-${HOME}/usr/bin/docker-compose}"
+# Downloads the latest docker-compose binary and verifies the checksum.  This
+# script is idempotent.  It will install docker-compose only if it is not the
+# latest.
 
 function latest_tag() {
-  curl -sfI https://github.com/docker/compose/releases/latest |
+  curl -sfI "${RELEASES}"/latest |
   tr '\r' '\n' |
   awk '$1 == "Location:" { gsub(/^.*\//, "", $2);print $2 }'
 }; declare -rf latest_tag
 
 # get the checksum for the downloaded binary
 function checksum() {
-  curl -sfL "https://github.com/docker/compose/releases/download/${TAG}/${BINARY}.sha256" |
+  curl -sfL "${RELEASES}/download/${TAG}/${CHECKSUM_FILE}" |
   awk "\$0 ~ /${BINARY}\$/ { print \$1 }"
 }; declare -rf checksum
 
-function sha256sum() {
+function sha256sum() (
   if type -P sha256sum > /dev/null; then
     command sha256sum "${@}"
   elif type -P shasum > /dev/null; then
@@ -25,15 +25,18 @@ function sha256sum() {
     echo "ERROR: could not find a sha256sum program."
     exit 1
   fi
-}; declare -rf sha256sum
+); declare -rf sha256sum
 
 set -auxeEo pipefail
 
-if [ ! -x "${DESTINATION}" ]; then
-  TAG="$( latest_tag )"
-  BINARY="docker-compose-$(uname -s)-$(uname -m)"
-
-  curl -fLo "${DESTINATION}" "https://github.com/docker/compose/releases/download/${TAG}/${BINARY}"
+DESTINATION="${DESTINATION:-${HOME}/usr/bin/docker-compose}"
+RELEASES="https://github.com/docker/compose/releases"
+TAG="$( latest_tag )"
+BINARY="docker-compose-$(uname -s)-$(uname -m)"
+CHECKSUM_FILE="${BINARY}.sha256"
+if [ ! -x "${DESTINATION}" ] ||
+    ! sha256sum -c - <<< "$(checksum)  ${DESTINATION}"; then
+  curl -fLo "${DESTINATION}" "${RELEASES}/download/${TAG}/${BINARY}"
   sha256sum -c - <<< "$(checksum)  ${DESTINATION}"
   chmod 755 "${DESTINATION}"
 fi
