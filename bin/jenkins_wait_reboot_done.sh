ba7462f8dc864cf77943a15d4307491847906115
Sam Gleske <sam.mxracer@gmail.com>
2018-11-27 23:59:03 -0800

    Bugfix jenkins wait reboot script

--- bin/jenkins_wait_reboot_done.sh
+++ bin/jenkins_wait_reboot_done.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 [ -e "${HOME}/.jenkins-bashrc" ] && source "${HOME}/.jenkins-bashrc"
 url="${1:-${JENKINS_WAIT_REBOOT_DEFAULT}}"
+url="${url%/}"
+if [ "${url##*/}" != login ]; then
+  url="${url}/login"
+fi
 endpoint=${url#*//}
 endpoint=${endpoint%%.*}
-while [ "503" = "$(curl ${CURL_OPTS} -siI -w "%{http_code}\\n" -o /dev/null ${url})" ];do echo -n '.';sleep 1;done
+endpoint=${endpoint%%:*}
+while [ ! 200 = "$(curl ${CURL_OPTS} -siI -w "%{http_code}\\n" -o /dev/null ${url})" ];do echo -n '.';sleep 1;done
 say_job_done.sh "${endpoint} ready."
