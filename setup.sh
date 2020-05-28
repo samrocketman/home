475558d3716fe7f8278091ab245e289942705409
Sam Gleske <sam.mxracer@gmail.com>
2020-05-28 02:54:21 -0400

    Update setup.sh to exclude authordomains
    Previously provided by hooks

--- setup.sh
+++ setup.sh
@@ -48,36 +48,19 @@ if ! git config --global -l | grep 'include.path=~/\.gitconfig_settings' &> /dev
   echo 'Adding include.path=~/.gitconfig_settings to git settings.'
   git config --global --add include.path '~/.gitconfig_settings'
 fi
-#configure authordomains in git
-if ! git config --global --bool authordomains.enabled &> /dev/null; then
-  echo 'Enable authordomains.'
-  git config --global authordomains.enabled true
-  for x in github.com gitlab.com git.gnome.org; do
-    if ! git config --global -l | grep "^authordomains.${x}" &> /dev/null; then
-      echo "Setting authordomains.${x}"
-      git config --global "authordomains.${x}.name" 'Sam Gleske'
-      git config --global "authordomains.${x}.email" 'sam.mxracer@gmail.com'
-    fi
-  done
-fi
-#copy pre-commit hook into existing git repositories.
-#but only if templates are in use
-if [ -e "${HOME}/git" -a -n "$(git config --get init.templatedir)" ]; then
-  (
-    TEMPLATEDIR="$(eval "echo $(git config --get init.templatedir)")"
-    cd "${HOME}/git"
-    find . -type d -name '.git' | while read x;do
-      if [ ! -e "${x}/hooks/pre-commit" ]; then
-        cp "${TEMPLATEDIR}/hooks/pre-commit" "${x}/hooks/"
-      fi
-    done
-  )
-fi
-git config --global authordomains.enabled false
 
 if [ ! -e "$HOME/bin" ]; then
   ln -s "$HOME/git/home/bin" "$HOME/bin"
 fi
+
+mkdir -p ~/usr/bin ~/usr/src
+(
+  if [ ! -e ~/usr/bin/git-idm ]; then
+    cd ~/usr/src
+    git clone https://github.com/samrocketman/git-identity-manager
+    ln -s ~/usr/src/git-identity-manager/git-idm ~/usr/bin/git-idm
+  fi
+)
 
 #
 # RASPBERRY PI ONLY SETUP
