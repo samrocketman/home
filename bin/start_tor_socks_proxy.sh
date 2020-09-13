4d32c8ac13900a23260f496a61e6e6f3a3ed92ff
Sam Gleske <sam.mxracer@gmail.com>
2020-09-13 11:22:03 -0400

    Support for more advanced option handling
    New Options:
      - Restricting TOR exit node to a specific country.
      - Connect through a strict outbound firewall.

--- bin/start_tor_socks_proxy.sh
+++ bin/start_tor_socks_proxy.sh
@@ -15,8 +15,70 @@
 #    */5 * * * * start_tor_socks_proxy.sh restart
 #    @reboot start_tor_socks_proxy.sh
 
+function helpdoc() {
+cat <<'EOF'
+SYNOPSIS
+    $0 [-f] [-c CODE] [restart]
 
-if [ "${1:-}" = restart ]; then
+BASIC OPTIONS
+  -f or --strict-firewall
+    TOR will only make outbound connections over ports 443 or 80 when
+    connecting to the onion network.
+
+  restart
+    Will kill an already running TOR docker container before starting.  Without
+    this option, an already running TOR docker container will make this command
+    a no-op.
+
+OPTIONS WITH ARGUMENTS
+  -c CODE or --country CODE
+    Provide a country CODE to restrict TOR exit notes just to that country.
+    Useful if you want to restrict your traffic to be coming from a specific
+    country.  If you're looking for a compromise between anonymity and speed,
+    then restricting exit nodes to your own country is more performant than no
+    restriction.
+EOF
+  exit 1
+}
+
+stopproxy=false
+country=""
+strict_firewall=false
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    restart)
+      stopproxy=true
+      shift
+      ;;
+    --country|-c)
+      if [ -z "${country:-}" ]; then
+        country="$2"
+      else
+        country+=",$2"
+      fi
+      shift
+      shift
+      ;;
+    --strict-firewall|-f)
+      strict_firewall=true
+      shift
+      ;;
+    *)
+      helpdoc
+      ;;
+  esac
+done
+
+while read c; do
+  echo $c
+  if [ -z "${country_config:-}" ]; then
+    country_config="{${c}}"
+  else
+    country_config+=",{${c}}"
+  fi
+done <<< "$(tr ',' '\n' <<< "${country}")"
+
+if [ true = "${stopproxy}" ]; then
   docker rm -f tor-socks-proxy
 fi
 
@@ -26,9 +88,26 @@ if docker ps -a | grep tor-socks-proxy; then
   exit
 fi
 
+# https://2019.www.torproject.org/docs/tor-manual.html.en
+# MiddleNodes is an experimental option and may be removed.
 docker run -d --restart=always --name tor-socks-proxy \
   -p 127.0.0.1:9150:9150/tcp \
   -p 127.0.0.1:53:53/udp \
-  peterdavehello/tor-socks-proxy:latest
+  --init \
+  peterdavehello/tor-socks-proxy:latest \
+  /bin/sh -exc "
+echo > /etc/tor/torrc2
+if [ '${strict_firewall}' = true ]; then
+  echo 'FascistFirewall 1' >> /etc/tor/torrc2
+  echo 'ReachableAddresses *:80,*:443' >> /etc/tor/torrc2
+fi
+if [ -n '${country_config}' ]; then
+  echo 'GeoIPExcludeUnknown 1' >> /etc/tor/torrc2
+  echo 'ExitNodes ${country_config}' >> /etc/tor/torrc2
+  echo 'MiddleNodes ${country_config}' >> /etc/tor/torrc2
+fi
+/usr/bin/tor  --defaults-torrc /etc/tor/torrc -f /etc/tor/torrc2
+"
+
 
 echo 'Started a new proxy.'
