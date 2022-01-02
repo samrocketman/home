df408b99cc4b7a7a8beb71c9e66bb2e076153f5e
Sam Gleske <sam.mxracer@gmail.com>
2022-01-02 12:57:13 -0500

    A convenient protontricks wrapper
    A wrapper for a wrapper for a wrapper

---
+++ bin/protontricks-wrapper.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Created by Sam Gleske
+# Sun Jan  2 12:53:44 EST 2022
+# Ubuntu 18.04.6 LTS
+# Linux 5.4.0-91-generic x86_64
+# GNU bash, version 4.4.20(1)-release (x86_64-pc-linux-gnu)
+# Python 3.6.9
+
+set -eo pipefail
+
+if [ ! -d ~/usr/share/protontricks-venv ]; then
+  mkdir -p ~/usr/share
+  python3 -m venv ~/usr/share/protontricks-venv
+fi
+
+source ~/usr/share/protontricks-venv/bin/activate
+
+if ! type -P protontricks &> /dev/null; then
+  pip install protontricks
+fi
+
+protontricks "$@"
