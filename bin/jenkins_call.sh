90f87394ba523f899a3c609923325c03f85ba90e
Sam Gleske <sam.mxracer@gmail.com>
2022-10-23 04:10:58 -0400

    Bugfix older python3 and py2.7 output

--- bin/jenkins_call.sh
+++ bin/jenkins_call.sh
@@ -4,13 +4,24 @@
 # appropriate way to call jenkins-call-url.  This enables a better user
 # experience by not having to worry about python version.
 
-if type -P python3 &> /dev/null; then
-  jenkins-call-url "$@"
-elif python -c 'import platform,sys; sys.exit(0) if platform.python_version().startswith("2.7") else sys.exit(1)'; then
-  python "$(type -P jenkins-call-url-2.7)" "@"
-elif python -c 'import platform,sys; sys.exit(0) if platform.python_version().startswith("3") else sys.exit(1)'; then
-  python "$(type -P jenkins-call-url)" "@"
+function minimum() (
+  exec &> /dev/null
+  local major="${2%.*}"
+  local minor="${2#*.}"
+  if ! type -P "$1"; then
+    return false
+  fi
+  python -c 'import platform,sys; check=lambda x,y,z: x.startswith(y) and int(x.split(".")[0:2][-1]) >= z; sys.exit(0) if check(platform.python_version(), sys.argv[1], int(sys.argv[2])) else sys.exit(1)' \
+  "${major}" "${minor}"
+)
+
+if minimum python3 '3.8'; then
+  "${SCRIPT_LIBRARY_PATH}"/jenkins-call-url "$@"
+elif minimum python '3.8'; then
+  python "${SCRIPT_LIBRARY_PATH}"/jenkins-call-url "$@"
+elif minimum python '2.7'; then
+  "${SCRIPT_LIBRARY_PATH}"/jenkins-call-url-2.7 "$@"
 else
-  echo 'Python 2 or 3 could not be detected.' >&2
+  echo 'Python 2 or 3.8+ could not be detected.' >&2
   exit 1
 fi
