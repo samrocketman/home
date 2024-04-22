35039c5cd25828feb8ee32aa1ff6165677a2a3c9
Sam Gleske <sam.mxracer@gmail.com>
2024-04-21 21:52:53 -0400

    Update utilities

--- misc/checksum.sh
+++ misc/checksum.sh
@@ -5,4 +5,8 @@ set -euo pipefail
 cd ~/git/github/home/misc
 
 download-utilities.sh --update
-download-utilities.sh --checksum -I Darwin:arm64 -I Darwin:x86_64 -I Linux:aarch64 -I Linux:x86_64
+download-utilities.sh --checksum \
+  -I Darwin:arm64 \
+  -I Darwin:x86_64 \
+  -I Linux:aarch64 \
+  -I Linux:x86_64
