2fd28476de9f3241462ed4472983e3df7cac9f5f
Sam Gleske <sam.mxracer@gmail.com>
2025-02-13 20:31:23 -0500

    Trigger relay after IR sense

--- raspi/garage-monitor.py
+++ raspi/garage-monitor.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-# Script to detect if garage door is open by IR sensors.
+# Script to detect if garage door is open by IR sensors.  If the door is open,
+# then it triggers a relay for an external notification system like Zooz
+# universal relay for garage doors.
 
 import RPi.GPIO as GPIO
 import time
@@ -17,8 +19,8 @@ automation_frequency=2
 # pins by function
 #
 # Flow:
-#     S1_ir -> S1_z -> G1 (garage door 1)
-#     S2_ir -> S2_z -> G2 (garage door 2)
+#     S1_ir -> R1_z -> G1 (garage door 1)
+#     S2_ir -> R2_z -> G2 (garage door 2)
 #
 # reading garage door status from IR sensors
 # pins: 14 (door 1 active), 18 (door 2 active), -1 (door disabled/skipped)
@@ -26,29 +28,43 @@ s1_garage_ir = 14
 s2_garage_ir = -1
 #s2_garage_ir = 18
 # for notifying the zwave universal relay
-s1_z_relay = 23
-s2_z_relay = 25
+r1_z_relay = 23
+r2_z_relay = 25
 
 ################################################################################
 # Functions
 ################################################################################
 
 def signal_handler(sig, frame):
+    """
+    Handles graceful shutdown of GPIO when the daemon is stopped.
+    """
     GPIO.cleanup()
     print('Cleanly shut down GPIO.')
     sys.exit(0)
 
 
 def set_input_pins(pins):
+    """
+    IR sensors require pull-up resistors which are built-in for the Raspberry
+    Pi Zero 2W.
+    """
     for pin in pins:
-        GPIO.setup(pin, GPIO.IN, GPIO.PUD_UP)
+        if pin < 0:
+            continue
+        GPIO.setup(channel=pin, direction=GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def set_output_pins(pins):
+    """
+    Prepare pins for 3.3v output for triggering 12v relays.
+    """
     for pin in pins:
-        GPIO.setup(pin, GPIO.OUT)
-        output_low(pin)
+        GPIO.setup(channel=pin, direction=GPIO.OUT, initial=GPIO.LOW)
 
 def initialize():
+    """
+    Prepares GPIO for work and graceful shutdown of GPIO program termination.
+    """
     # BCM mode pins 0-8 are pull-up and the rest are pull down.
     GPIO.setmode(GPIO.BCM)
     signal.signal(signal.SIGHUP, signal_handler)
@@ -56,27 +72,42 @@ def initialize():
     signal.signal(signal.SIGTERM, signal_handler)
 
 def activate(pin):
+    """
+    Sends high output (3.3v) to GPIO pin.
+    """
     GPIO.output(pin, True)
 
 def deactivate(pin):
+    """
+    Sends low output (~0v) to GPIO pin.
+    """
     GPIO.output(pin, False)
 
-def is_beam_broken(pin):
-    return GPIO.input(pin) == GPIO.LOW
+def is_beam_broken_for(ir_sensor_pin):
+    """
+    Assumes a digital break-beam IR sensor is plugged into the pin.  If the pin
+    reads high, then the IR beam is intact.  If the pin reads low, then the IR
+    beam is broken.
+
+    Garage door open == broken beam and garage door closed == intact beam.
+
+    Function returns True if beam is broken i.e. garage door open.
+    """
+    return GPIO.input(ir_sensor_pin) == GPIO.LOW
 
 ################################################################################
 # Main execution
 ################################################################################
 initialize()
 set_input_pins([s1_garage_ir, s2_garage_ir])
-set_output_pins([s1_z_relay, s2_z_relay])
-pin_pairs = [(s1_garage_ir, s1_z_relay), (s2_garage_ir, s2_z_relay)]
+set_output_pins([r1_z_relay, r2_z_relay])
+pin_pairs = [(s1_garage_ir, r1_z_relay), (s2_garage_ir, r2_z_relay)]
 print('Monitoring GPIO pins.')
 while True:
     for (ir_sensor, relay) in pin_pairs:
         if ir_sensor < 0:
             continue
-        if is_beam_broken(ir_sensor):
+        if is_beam_broken_for(ir_sensor):
             activate(relay)
         else:
             deactivate(relay)
