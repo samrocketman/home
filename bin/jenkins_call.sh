4d89840e6cbe9016ed2c3f132149983a6bf87b98
Sam Gleske <sam.mxracer@gmail.com>
2025-03-13 00:14:08 -0400

    Fix return error in jenkins_call.sh

--- bin/jenkins_call.sh
+++ bin/jenkins_call.sh
@@ -9,7 +9,7 @@ function minimum() (
   local major="${2%.*}"
   local minor="${2#*.}"
   if ! type -P "$1"; then
-    return false
+    return 1
   fi
   python -c 'import platform,sys; check=lambda x,y,z: x.startswith(y) and int(x.split(".")[0:2][-1]) >= z; sys.exit(0) if check(platform.python_version(), sys.argv[1], int(sys.argv[2])) else sys.exit(1)' \
   "${major}" "${minor}"
