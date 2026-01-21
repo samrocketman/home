6fb3d2e5954bcc2e670cb1e246f46c7580dc86d6
Sam Gleske <sam.mxracer@gmail.com>
2026-01-20 22:05:20 -0500

    Detect jenkins URL from headers file when auth

--- bin/jenkins_auth.sh
+++ bin/jenkins_auth.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-jenkins_call.sh -avvo /dev/null -m HEAD "$1"
+url="${1:-}"
+if [ -z "${url:-}" ]; then
+  if [ -f "${JENKINS_HEADERS_FILE:-}" ]; then
+    jenkins_host="$(yq .headers.Host < "$JENKINS_HEADERS_FILE")"
+    url="https://${jenkins_host}/"
+  fi
+fi
+jenkins_call.sh -avvo /dev/null -m HEAD "${url}"
