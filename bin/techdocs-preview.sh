be87aef6a55e0a904f58806bed0c6e8cb3334920
Sam Gleske <sam.mxracer@gmail.com>
2026-02-24 22:37:41 -0500

    Clean up live-edit plugin

--- bin/techdocs-preview.sh
+++ bin/techdocs-preview.sh
@@ -43,49 +43,12 @@ install_techdocs() (
     griffe==1.6.0
 
   # live-edit
-  pip install websockets==16.0 mkdocs-live-edit-plugin==0.3.1
-  patch_live_edit_plugin
+  pip install websockets==16.0 git+https://github.com/samrocketman/mkdocs-live-edit-plugin
 )
 
-# Fix mkdocs-live-edit-plugin bug: config_scheme uses Type(string) instead of Type(str).
-# string is the string module, not a type - causes "isinstance() arg 2 must be a type" on Python 3.13.
-patch_live_edit_plugin() {
-  # shellcheck disable=SC1090
-  source ~/.techdocs/python3/bin/activate
-  python3 -c "
-import pathlib
-try:
-    import live.plugin
-    path = pathlib.Path(live.plugin.__file__)
-    text = path.read_text()
-    if 'Type(string,' in text:
-        path.write_text(text.replace('Type(string,', 'Type(str,'))
-        print('Patched mkdocs-live-edit-plugin: Type(string) -> Type(str)', file=__import__('sys').stderr)
-except Exception as e:
-    print(f'Could not patch live-edit plugin: {e}', file=__import__('sys').stderr)
-" 2>/dev/null || true
-}
-
-open_url() (
-  {
-    if type -P nc &> /dev/null; then
-      count=0
-      until {
-        nc -vz "${TECHDOCS_HOST}" "${TECHDOCS_PORT}" &> /dev/null ||
-          [ "$count" -gt 5 ]
-      }; do
-        sleep 1
-        count="$(( count + 1 ))"
-      done
-    else
-      sleep 5
-    fi
-  } && open http://"${TECHDOCS_HOST}:${TECHDOCS_PORT}"/
-)
 serve() (
   # shellcheck disable=SC1090
   source ~/.techdocs/python3/bin/activate
-  [ -n "${LIVE_EDIT:-}" ] && patch_live_edit_plugin
   mkdocs_config > "${TMP_DIR}"/mkdocs.yml
   mv "${TMP_DIR}"/mkdocs.yml ./
   TMPDIR="${TMP_DIR}" mkdocs serve \
@@ -103,59 +66,6 @@ build() (
     mkdocs build -f - -t material "$@"
 )
 
-# Work around bugs
-# https://github.com/EddyLuten/mkdocs-live-edit-plugin/issues/18
-# https://github.com/backstage/mkdocs-monorepo-plugin/issues/151
-# https://github.com/EddyLuten/mkdocs-live-edit-plugin/issues/3 - MacOS: websockets_host=127.0.0.1 avoids IPv6 ::1 bind failure
-liveedit_or_techdocs_plugin() {
-  if [ -n "${LIVE_EDIT:-}" ]; then
-cat <<EOF
-  - live-edit:
-      websockets_host: ${TECHDOCS_HOST}
-      websockets_port: ${TECHDOCS_WEBSOCKET_PORT}
-
-# https://github.com/backstage/mkdocs-techdocs-core/blob/main/techdocs_core/core.py
-# emulate techdocs-core to avoid monorepo plugin
-markdown_extensions:
-  - toc:
-      permalink: true
-  - pymdownx.blocks.admonition:
-      types: [ "new", "settings", "note", "abstract", "info", "tip", "success", "question", "warning", "failure", "danger", "bug", "example", "quote" ]
-  - pymdownx.blocks.details:
-      types: [ {"name": "details-new", "class": "new"}, {"name": "details-settings", "class": "settings"}, {"name": "details-note", "class": "note"}, {"name": "details-abstract", "class": "abstract"}, {"name": "details-info", "class": "info"}, {"name": "details-tip", "class": "tip"}, {"name": "details-success", "class": "success"}, {"name": "details-question", "class": "question"}, {"name": "details-warning", "class": "warning"}, {"name": "details-failure", "class": "failure"}, {"name": "details-danger", "class": "danger"}, {"name": "details-bug", "class": "bug"}, {"name": "details-example", "class": "example"}, {"name": "details-quote", "class": "quote"} ]
-  - pymdownx.blocks.tab:
-      alternate_style: true
-  - pymdownx.caret
-  - pymdownx.critic
-  - pymdownx.emoji:
-      emoji_generator: !!python/name:pymdownx.emoji.to_svg
-  - pymdownx.inlinehilite
-  - pymdownx.magiclink
-  - pymdownx.mark
-  - pymdownx.smartsymbols
-  - pymdownx.snippets
-  - pymdownx.highlight:
-      linenums: true
-      pygments_lang_class: true
-  - pymdownx.extra
-  - pymdownx.betterem:
-      smart_enable: all
-  - pymdownx.tasklist:
-      custom_checkbox: true
-  - pymdownx.tilde
-  - markdown_inline_graphviz
-  - plantuml_markdown
-  - mdx_truly_sane_lists
-EOF
-  else
-cat <<EOF
-  - techdocs-core:
-      use_material_search: true
-      use_pymdownx_blocks: true
-EOF
-  fi
-}
-
 mkdocs_config() {
 cat <<EOF
 $(cat mkdocs.yml)
@@ -168,7 +78,11 @@ plugins:
         - stemmer
         - stopWordFilter
         - trimmer
-$(liveedit_or_techdocs_plugin)
+  - techdocs-core:
+      use_material_search: true
+      use_pymdownx_blocks: true
+  - live-edit:
+      user_docs_dir: "${PWD}/docs"
 EOF
 }
 
@@ -187,7 +101,6 @@ SYNOPSIS
   techdocs-preview.sh build --help
   techdocs-preview.sh build [additional mkdocs options]
   techdocs-preview.sh serve --help
-  techdocs-preview.sh serve static
   techdocs-preview.sh serve [additional mkdocs options]
 
 DESCRIPTION
@@ -195,17 +108,11 @@ DESCRIPTION
   environment.
 
   With no options "serve" is the default and a browser link will be opened.
-
-  techdocs-preview.sh serve static
-    Disables live-edit plugin and instead relies on mkdocs-techdocs-core plugin.
 EOF
     exit
     ;;
 esac
 install_techdocs
-#if [ "$#" = 0 ]; then
-#  open_url &
-#fi
 if [ "${1:-}" = install ]; then
   install_techdocs
 elif [ "${1:-}" = build ]; then
@@ -214,11 +121,6 @@ elif [ "${1:-}" = build ]; then
 else
   if [ "${1:-}" = serve ]; then
     shift
-  fi
-  if [ "${1:-}" = static ]; then
-    shift
-  else
-    export LIVE_EDIT=1
   fi
   serve "$@"
 fi
